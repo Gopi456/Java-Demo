17271092b52b893a391d1875ba42c2cdb6cb6a8a
GOPI REDDY ADAPA <109288410+Gopi456@users.noreply.github.com>
2025-03-03 19:26:15 +0530

    Update tech_number.java

--- tech_number.java
+++ tech_number.java
@@ -1,10 +1,6 @@
-class tech_number 
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello everyone");{
-			System.out.println("welcome to everyone")
-		}
-		
-	}
+class TechNumber {
+    public static void main(String[] args) {
+        System.out.println("Hello everyone");
+        System.out.println("Welcome to everyone");
+    }
 }
